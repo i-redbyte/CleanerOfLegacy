92e2eacb102fbc7163623242b1daf05a017cd28c
Ilya Sokolov <developer.sokolov@gmail.com>
2025-02-19 20:03:52 +0300

    add test file

---
+++ app/src/main/java/su/redbyte/KillFile.kt
@@ -0,0 +1,10 @@
+package su.redbyte
+
+import su.redbyte.cleaneroflegacy.annotation.RemoveAfter
+
+@RemoveAfter("1.0.0")
+class KillFile {
+    fun testFunc() {
+        // TODO: best function
+    }
+}
